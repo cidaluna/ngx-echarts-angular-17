755526b5b3aefbe288b9aebc11f8bdd3cb37de8a
Cida Luna <cidalunna@hotmail.com>
2025-01-27 21:03:26 -0300

    Update dashboard example charts with ngx-echarts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BarChartComponent } from "../bar-chart/bar-chart.component";
 import { LineBarComponent } from "../line-bar/line-bar.component";
+import { PieChartComponent } from "../pie-chart/pie-chart.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [BarChartComponent, LineBarComponent],
+  imports: [BarChartComponent, LineBarComponent, PieChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
